19323cb0c54c6463971a16341685ee625e2c46bb
th3_bu6 <th3_bu6@lap.huawei.net>
2018-03-21 11:04:58 -0600

    we implemented all the responses for the express modules

--- api.js
+++ api.js
@@ -9,9 +9,12 @@ api.get('/agents', (req, res) => {
   debug(' A request has come to /agents')
   res.send({})
 })
-api.get('/agent/:uuid', (req, res) => {
-  const {uuid} = req.params
-  res.send({ uuid })
+api.get('/agent/:uuid', (req, res, next) => {
+    const {uuid} = req.params
+    if(uuid !== 'yyy'){
+        return next(new Error('Agent not found'))
+    }
+    res.send({ uuid })
 })
 
 api.get('/metrics/:uuid', (req, res) => {
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const debug = require('debug')
 const http = require('http')
 const express = require('express')
 const chalk = require('chalk')
@@ -10,6 +11,26 @@ const port = process.env.PORT || 3000
 const server = http.createServer(app)
 
 app.use('/api', api)
+
+//Express Error handler
+
+app.use((err, req, res, next) => {
+    debug(`Error: ${err.message}`)
+
+    if(err.message.match(/not found/)){
+        return res.status(404).send({error: err.message})
+    }
+    res.status(500).send({ error: err.message})
+})
+
+function handleFatalError (err) {
+    console.error(`${chalk.red('[Fatal Error]')} ${err.message}`)
+    console.error(err.stack)
+    process.exit(1)
+}
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandleFatalError', handleFatalError)
 
 server.listen(port, () => {
   console.log(`${chalk.green('[bugverse-api]')} server listening on port ${port}`)
